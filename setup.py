e224a7f4c9ab007ceb710238eec6268e82aeda8b
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-10-29 23:25:22 -0200

    Add setup.py . Change module structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="pyBrows",
+    version='0.1.0',
+    author="VulcanoAhab",
+    packages=["pybrows"],
+    url="https://github.com/VulcanoAhab/pyBrows",
+    description="Browsers Utils",
+    install_requires=[
+
+        "requests==2.18.4",
+        "selenium==3.6.0",
+        "lxml==4.1.0",
+        ]
+)
