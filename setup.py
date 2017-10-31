ad2f975c54fdb17fd524b58757ab922b4fc6be3b
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-10-30 23:35:30 -0200

    Fix import paths

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
     name="pyBrows",
     version='0.1.0',
     author="VulcanoAhab",
-    packages=["pyBrows"],
+    packages=["pyBrows", "pyBrows.headers"],
     url="https://github.com/VulcanoAhab/pyBrows.git",
     description="Browsers Utils",
     install_requires=[
-
         "requests==2.18.4",
         "selenium==3.6.0",
         "lxml==4.1.0",
