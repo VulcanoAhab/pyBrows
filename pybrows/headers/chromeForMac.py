92958583dc5113548d4d090366b968b19a9cdbc4
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-11-08 15:16:59 -0200

    Fixing dir naming

--- pybrows/headers/chromeForMac.py
+++
@@ -1,6 +0,0 @@
-HEADERS={
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.8",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-}
