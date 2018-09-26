9c753710417827d0515476a28e2c8b6dbd73d148
VulcanoAhab <vulcano.ahab@gmail.com>
2018-09-25 23:50:27 -0300

    Adding pyppeterChromium

--- pyBrows/pyppeterChromium.py
+++ pyBrows/pyppeterChromium.py
@@ -15,7 +15,7 @@ class Headless(Interface):
         self._download=kwargs.get("downloadPath")
         self._user_data_dir=kwargs.get("user_data_dir")
         self._remote_debugging_port=kwargs.get("remote_debugging_port")
-        asyncio.get_event_loop().run_until_complete(self._startDriver())
+        self._startDriver()
 
     def _startDriver(self):
         """
